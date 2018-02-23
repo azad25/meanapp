ef75143bc88b27cfcfa4e5c7cc5cbff38892f6d5
Ferdous Azad <ferdousazad12@gmail.com>
2018-02-23 06:36:24 +0600

    User model added with schema and functions

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 //Register
-router.get('/register', (req,res,next)=>{
-    res.send('Register');
+router.post('/register', (req,res,next)=>{
+    let newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    User.addUser(newUser, (err,user) => {
+      if(err){
+        res.json({
+          succsess:false,
+          msg: 'Failed to register'
+        });
+      }else{
+        res.json({
+          succsess:true,
+          msg: 'Registered'
+        });
+      }
+    });
 });
+
 
 //Authenticate
 router.get('/authenticate', (req,res,next)=>{
